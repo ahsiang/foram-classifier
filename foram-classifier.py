81db640266d6d5e13a7e1b2ae4308c779aa3e08d
ahsiang <allison.hsiang@gmail.com>
2024-01-29 15:33:47 +0100

    Fixes for deprecation errors and version compatibility issues

--- foram-classifier.py
+++ foram-classifier.py
@@ -16,6 +16,7 @@ from keras.layers import Dropout, Flatten, Dense, GlobalAveragePooling2D
 from keras import backend as k
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 from sklearn.metrics import classification_report, confusion_matrix
+import tensorflow as tf
 
 # Structure of data directories expected
 # base_dir/
@@ -28,15 +29,11 @@ from sklearn.metrics import classification_report, confusion_matrix
 # |   |   ├── class2_train_img1.jpg
 # |   |   ├── class2_train_img2.jpg
 # |   |   └── ...
-# ├── validation/
-# |   ├── class1
-# |   |   ├── class1_val_img1.jpg
-# |   |   ├── class1_val_img2.jpg
-# |   |   └── ...
-# |   └── ...
-# └── weights/
-#     ├── weights_file_1.h5
-#     ├── weights_file_2.h5
+# └── validation/
+#     ├── class1
+#     |   ├── class1_val_img1.jpg
+#     |   ├── class1_val_img2.jpg
+#     |   └── ...
 #     └── ...
 #
 # Outputs the following files (with user-specified analysis ID prefixes):
@@ -45,11 +42,14 @@ from sklearn.metrics import classification_report, confusion_matrix
 #   predictions.pkl     pickle file containing predictions by model for all validation images
 #   confusion.pkl       pickle file containing raw data to generate confusion matrix
 #   labels.pkl          pickle file containing all class labels
-#
-# Note: Weights files can be downloaded at: https://github.com/fchollet/deep-learning-models/releases/
 
 # Get information on available GPUs in system
-k.tensorflow_backend._get_available_gpus()
+# k.tensorflow_backend._get_available_gpus() # Deprecated
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+	pass
+else:
+	raise Exception("No GPUs found.")
 
 # Set up top-3 accuracy metric
 top3_acc = functools.partial(keras.metrics.top_k_categorical_accuracy, k=3)
@@ -105,7 +105,7 @@ train_data_dir = os.path.join(base_dir,'train')
 validation_data_dir = os.path.join(base_dir,'validation')
 
 # Determine number of steps required to send all validation/training images
-# through one forward propogation. Note that if modulo(number of samples / batch size) != 0,
+# through one forward propagation. Note that if modulo(number of samples / batch size) != 0,
 # an extra step is required to pass the remainder images through the network
 if num_validation_samples % batch_size == 0:
     validation_steps = num_validation_samples // batch_size
@@ -121,8 +121,7 @@ else:
 # include_top is False because we want to add change the size of the final fully-connected
 # layer to match the number of classes in our specific problem
 if cnn == 'vgg16':
-    weights = os.path.join(base_dir,'weights','vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5')
-    model = applications.VGG16(weights=weights, include_top=False, input_shape = (img_width, img_height, 3))
+    model = applications.VGG16(weights='imagenet', include_top=False, input_shape = (img_width, img_height, 3))
     layer_freeze = 7
 
 if cnn == 'inceptionv3':
@@ -164,7 +163,7 @@ predictions = Dense(num_classes, activation="softmax")(x)
 # Finally, we connect the input model layers with the output fully-connected layer and compile
 model_final = Model(inputs = model.input, outputs = predictions)
 model_final.summary()
-model_final.compile(loss = "categorical_crossentropy", optimizer = optimizers.Adam(lr=lrate), metrics=['accuracy',top3_acc])
+model_final.compile(loss = "categorical_crossentropy", optimizer = optimizers.Adam(learning_rate=lrate), metrics=['accuracy',top3_acc])
 
 # Data augmentation (if set) and data generators to read and process training/validation images
 if augment:
@@ -196,7 +195,7 @@ train_generator = train_datagen.flow_from_directory(
 validation_generator = test_datagen.flow_from_directory(
         validation_data_dir,
         target_size = (img_height, img_width),
-	batch_size = batch_size,
+		batch_size = batch_size,
         class_mode = "categorical")
 
 # Create output directory if it doesn't exist
@@ -204,16 +203,16 @@ if not os.path.exists(output_dir):
     os.mkdir(output_dir)
 
 # Set up checkpointing (saves best-performing model weights after each epoch)
-checkpoint = ModelCheckpoint(os.path.join(output_dir,'analysis_{:s}_checkpoint.h5'.format(analysis_id)),
-                             monitor='val_acc',
+checkpoint = ModelCheckpoint(os.path.join(output_dir,'analysis_{:s}_checkpoint.keras'.format(analysis_id)),
+                             monitor='val_accuracy',
                              verbose=1,
                              save_best_only=True,
                              save_weights_only=False,
                              mode='auto',
-                             period=1)
+                             save_freq="epoch")
 
 # Set up early stopping monitor (will stop run if validation accuracy doesn't improve for 10 epochs)
-early = EarlyStopping(monitor='val_acc',
+early = EarlyStopping(monitor='val_accuracy',
                       min_delta=0,
                       patience=10,
                       verbose=1,
@@ -221,7 +220,7 @@ early = EarlyStopping(monitor='val_acc',
 
 # Set up automatic learning rate adjustment if requested
 if adjust_lrate:
-    reduceLR = ReduceLROnPlateau(monitor = 'val_acc',factor=0.5,
+    reduceLR = ReduceLROnPlateau(monitor = 'val_accuracy',factor=0.5,
                                    patience = 3, verbose = 1, mode = 'auto',
                                    min_delta = 0.005, min_lr = 0.00001)
     callbacks = [checkpoint, early, reduceLR]
@@ -229,7 +228,7 @@ else:
     callbacks = [checkpoint, early]
 
 # Run model training using generator
-history = model_final.fit_generator(
+history = model_final.fit(
             train_generator,
             steps_per_epoch = train_steps,
             epochs = epochs,
@@ -238,14 +237,14 @@ history = model_final.fit_generator(
             callbacks = callbacks)
 
 # Save best-performing model
-model_final.save(os.path.join(output_dir,'analysis_{:s}_model.h5'.format(analysis_id)))
+model_final.save(os.path.join(output_dir,'analysis_{:s}_model.keras'.format(analysis_id)))
 
 # Save histories
 with open(os.path.join(output_dir,'analysis_{:s}_history.pkl'.format(analysis_id)), 'wb') as f:
     pickle.dump(history.history,f)
 
 # Save confusion matrix, classification report, and label map
-Y_pred = model_final.predict_generator(validation_generator, steps=validation_steps)
+Y_pred = model_final.predict(validation_generator, steps=validation_steps)
 y_pred = np.argmax(Y_pred, axis=1)
 confusion = confusion_matrix(validation_generator.classes, y_pred)
 label_map = (validation_generator.class_indices)
